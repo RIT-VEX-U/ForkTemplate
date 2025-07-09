bc854ea4f8285126cd1dff101ca0b956d12cf377
Jack Cammarata <jackcammarata1@gmail.com>
2025-07-09 16:01:07 -0400

    clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       unknown                                                   */
-/*    Created:      12/1/2024, 11:02:31 AM                                    */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
 #include "vex.h"
 #include "robot-config.h"
 #include "competition/autonomous.h"
