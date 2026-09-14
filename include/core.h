cf5c9caa77edf3457a77fbe1555518105f322b67
DylanSward <dylansward06@gmail.com>
2026-09-14 18:33:53 -0400

    Will is suffering from dementia

--- include/core.h
+++ include/core.h
@@ -2,6 +2,12 @@
 
 // Device package
 #include "core/device/cobs_device.h"
+#include "core/device/vdb/builtins.hpp"
+#include "core/device/vdb/crc32.hpp"
+#include "core/device/vdb/protocol.hpp"
+#include "core/device/vdb/registry-controller.hpp"
+#include "core/device/vdb/types.hpp"
+#include "core/device/vdb/visitor.hpp"
 #include "core/device/wrapper_device.hpp"
 
 // Subsystems package
